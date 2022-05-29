42e91d2693fc92f4a0cf6fdc8f96f10a6c3ce649
alamkamajana <alamkamajana@gmail.com>
2022-05-29 17:14:20 +0800

    Menyelesaikan Project

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name : "Monica",
+    gender : "Female",
+    age : 17,
+    email : "Monica@dingdong.com",
+    favouriteColor : ["Yellow","Purple","Pink","White"],
+    isHavePet : true,
+    education : [{
+        name : "SD 01",
+        city : "Jakarta",
+        graduate : 2016
+    },{
+        name : "SMP 02",
+        city : "Jakarta",
+        graduate : 2019
+    },{
+        name : "SMA 03",
+        city : "Tangerang"
+    }],
+    favoriteRestaurant : ["Bento","Sushi","Pancake","Eggy","Tempura","Padang"]
+};
+const secondUser = {
+    name : "Wendy",
+    gender : "Male",
+    age : 23,
+    email : "Wendy@Dingdong.com",
+    favouriteColor : ["Blue","Black","Grey"],
+    isHavePet : false,
+    education : [{
+        name : "SD 02",
+        city : "Jakarta",
+        graduate : 2010
+    },{
+        name : "SMA 01",
+        city : "Surabaya",
+        graduate : 2016
+    },{
+        name : "Universitas Maju",
+        city : "Tangerang"
+    }],
+    favoriteRestaurant : ["Bakso","Sushi","Pancake","Eggy","Tempura","Padang"]
+};
+
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser,secondUser];
+
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
