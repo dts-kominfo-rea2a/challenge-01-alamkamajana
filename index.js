9bd4fa5f9b8da9f594d07955b6c49e72f7bde7e3
Made Alam Kamajana <Alam@Administrators-MacBook-Pro.local>
2022-05-30 18:44:23 +0800

    modified index.js

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ const secondUser = {
 
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser,secondUser];
+const users = [];
+users.push(firstUser)
+users.push(secondUser)
 
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
